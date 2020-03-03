0a0b88ad962a3b8fad084a8232a1afa36b1fbe8c
iamsal <sheikhsalman2013@gmail.com>
2020-03-03 20:32:30 +0600

    added simulation table and outputs

--- S&M_solver/script.js
+++ S&M_solver/script.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
 var TBA_table=$('#SQC-TBA-output-table').DataTable({
     responsive: true,
-    ordering:true,
     pageLength:5,
     searching:true,
     paginate:false,
@@ -10,4 +9,28 @@ var TBA_table=$('#SQC-TBA-output-table').DataTable({
 
 });
     new $.fn.dataTable.FixedHeader(TBA_table);
+
+
+    var ST_table=$('#SQC-ST-output-table').DataTable({
+        responsive: true,
+        pageLength:5,
+        searching:true,
+        paginate:false,
+        scrollY:290,
+        bInfo : false,
+
+    });
+    new $.fn.dataTable.FixedHeader(ST_table);
+
+
+    var Simulation_table=$('#SQC-Simulation-output-table').DataTable({
+        responsive: true,
+        searching:true,
+        paginate:true,
+        scrollY:400,
+        scrollCollapse: true,
+        bInfo : false,
+
+    });
+    new $.fn.dataTable.FixedHeader(Simulation_table);
 });
